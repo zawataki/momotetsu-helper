8ed31725a3ad6b2635696e13bb56f330a2137134
Yuki Takizawa <y.takizawa64@gmail.com>
2021-07-22 23:36:16 +0900

    Remove unused logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        桃鉄ヘルパー for "桃太郎電鉄 ～昭和 平成 令和も定番！～"
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
